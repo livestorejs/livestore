c8a0364dc986a69a58273a7ec1fb5a854db2a958
Johannes Schickling <schickling.j@gmail.com>
2023-10-04 23:21:52 +0200

    new error

--- packages/@livestore/utils/src/effect/Error.ts
+++ packages/@livestore/utils/src/effect/Error.ts
@@ -1,8 +1,11 @@
-import { Data } from 'effect'
+import { Error } from 'effect'
 
 import { errorToString, objectToString } from '../index.js'
 
-export class UnknownError extends Data.TaggedClass('UnknownError')<{ readonly error: any; readonly payload?: any }> {
+export class UnknownError extends Error.TaggedClass('UnknownError')<{
+  readonly error: any
+  readonly payload?: any
+}> {
   toString = () => {
     const payloadStr = this.payload ? ` with payload ${objectToString(this.payload)}` : ''
     return `UnknownError: ${errorToString(this.error)}${payloadStr}`
