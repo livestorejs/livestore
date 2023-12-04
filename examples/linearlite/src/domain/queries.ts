638034aa3c894efb444a6f7fa622ada9cb129f27
Matt <1009003+tantaman@users.noreply.github.com>
2023-12-04 09:22:03 -0500

    fix lint by moving share query to a queries file

---
+++ examples/linearlite/src/domain/queries.ts
@@ -0,0 +1,10 @@
+import { querySQL, sql } from '@livestore/livestore'
+import { FilterState } from './schema'
+
+export const filterState$ = querySQL<{ value: string }>(
+  (_) => sql`SELECT * FROM app_state WHERE "key" = 'filter_state'`,
+)
+  .getFirstRow({
+    defaultValue: { value: '{}' },
+  })
+  .pipe<FilterState>((row) => JSON.parse(row.value))
