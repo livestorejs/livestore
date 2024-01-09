b8965260c247e7093f44501cfea161a2d555f8ca
Nik Graf <nik@nikgraf.com>
2024-01-09 13:04:43 +0100

    add expo-sqlite/next

--- examples/expo-app/App.tsx
+++ examples/expo-app/App.tsx
@@ -1,9 +1,30 @@
+import * as SQLite from 'expo-sqlite/next'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 import { schema } from './schema'
 
 console.log(schema)
+
+const load = async () => {
+  const db = await SQLite.openDatabaseAsync('databaseName')
+  console.log('DATABASE:', db)
+
+  db.execSync(`
+PRAGMA journal_mode = WAL;
+CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+INSERT INTO test (value, intValue) VALUES ('test1', 123);
+INSERT INTO test (value, intValue) VALUES ('test2', 456);
+INSERT INTO test (value, intValue) VALUES ('test3', 789);
+`)
+
+  const allRows = await db.getAllSync('SELECT * FROM test')
+  for (const row of allRows) {
+    console.log(row.id, row.value, row.intValue)
+  }
+}
+
+load()
 
 export default function App() {
   return (
