6da07b15d36707a13295468f0f0ca62c1bf6d00d
Johannes Schickling <schickling.j@gmail.com>
2024-05-09 15:11:12 +0200

    upgrade deps and remove effect ws workaround

--- examples/expo/babel.config.js
+++ examples/expo/babel.config.js
@@ -3,20 +3,6 @@ module.exports = (api) => {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'babel-plugin-transform-vite-meta-env',
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            // Effect depends on the ws package,
-            // that is not supported on React Native runtime.
-            // This alias replaces the ws to a client only ws lib,
-            // that works on React Native.
-            ws: require.resolve('./react-native-ws.js'),
-          },
-        },
-      ],
-    ],
+    plugins: ['babel-plugin-transform-vite-meta-env'],
   }
 }
--- examples/expo/react-native-ws.js
+++
@@ -1,6 +0,0 @@
-// A client only ws lib that works on React Native
-export const WebSocket = globalThis.WebSocket
-
-export default {
-  WebSocket,
-}
