347a0c0ca172bc6ec98f0bb9bdd6c0b7574cfb6e
Johannes Schickling <schickling.j@gmail.com>
2024-07-25 18:14:13 +0200

    trace

--- integration-tests/playwright.config.ts
+++ integration-tests/playwright.config.ts
@@ -54,7 +54,7 @@ const config: PlaywrightTestConfig = {
     screenshot: 'on',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
