58501e315b87421a6308aaa6efacdbefdd21a65a
Johannes Schickling <schickling.j@gmail.com>
2024-09-05 15:29:50 +0200

    adjust metro

--- examples/expo-app/metro.config.js
+++ examples/expo-app/metro.config.js
@@ -3,24 +3,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config')
 const { addLiveStoreDevtoolsMiddleware } = require('@livestore/devtools-expo')
-const path = require('node:path')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
 config.resolver.unstable_enableSymlinks = true
 config.resolver.unstable_enablePackageExports = true
-
-// Needed for monorepo setup (can be removed in standalone projects)
-const projectRoot = __dirname
-const monorepoRoot = path.resolve(projectRoot, '../..')
-
-config.watchFolders = [monorepoRoot]
-
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-]
 
 addLiveStoreDevtoolsMiddleware(config, { schemaPath: './src/schema/index.ts' })
 
