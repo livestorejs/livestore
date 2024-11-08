96503d23836197a1b8daaf2e1148062c8e898458
betomoedano <betomoedano@outlook.com>
2024-11-07 18:23:21 -0600

    upgrade example app to sdk 52

--- examples/expo-app/metro.config.js
+++ examples/expo-app/metro.config.js
@@ -3,6 +3,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config')
 const { addLiveStoreDevtoolsMiddleware } = require('@livestore/devtools-expo')
+const path = require('node:path')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
@@ -10,7 +11,30 @@ const config = getDefaultConfig(__dirname)
 config.resolver.unstable_enableSymlinks = true
 config.resolver.unstable_enablePackageExports = true
 
-addLiveStoreDevtoolsMiddleware(config, { schemaPath: './src/schema/index.ts' })
+// Needed for monorepo setup (can be removed in standalone projects)
+const projectRoot = __dirname
+const monorepoRoot = path.resolve(projectRoot, '../..')
+
+config.watchFolders = [monorepoRoot]
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+]
+
+if (!process.env.CI && process.stdout.isTTY) {
+  addLiveStoreDevtoolsMiddleware(config, {
+    schemaPath: './src/schema/index.ts',
+    viteConfig: (viteConfig) => {
+      viteConfig.server.fs ??= {}
+      // Point to Overtone monorepo root
+      viteConfig.server.fs.allow.push(process.env.WORKSPACE_ROOT + '/../..')
+      viteConfig.optimizeDeps.force = true
+
+      return viteConfig
+    },
+  })
+}
 
 // console.log(config)
 module.exports = config
