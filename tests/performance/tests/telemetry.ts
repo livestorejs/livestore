5b93b726da4edcdda84cd7fe8248c0937a2bcfb0
Igor Gassmann <igor@igassmann.me>
2025-03-11 14:46:48 +0100

    test(perf): refactor telemetry layer

--- tests/performance/tests/telemetry.ts
+++ tests/performance/tests/telemetry.ts
@@ -1,4 +1,3 @@
-// src/metrics/telemetry.ts
 import type { Duration } from '@livestore/utils/effect'
 import { Context, Layer, Metric } from '@livestore/utils/effect'
 import { OtelLiveHttp } from '@livestore/utils/node'
@@ -16,36 +15,20 @@ export class PerformanceMetrics extends Context.Tag('PerformanceMetrics')<
   }
 >() {}
 
-const queryLatency = Metric.timer('query_latency', 'time taken to complete query operations')
-
-const mutationLatency = Metric.timer('mutation_latency', 'time taken to complete mutation operations')
-
-const memoryUsage = Metric.gauge('memory_usage', {
-  description: 'Memory usage during operations',
+const PerformanceMetricsLive = Layer.succeed(PerformanceMetrics, {
+  queryLatency: Metric.timer('query_latency', 'time taken to complete query operations'),
+  queryThroughput: Metric.counter('query_throughput', {
+    description: 'Rate at which query operations are processed',
+    incremental: true,
+  }),
+  mutationLatency: Metric.timer('mutation_latency', 'time taken to complete mutation operations'),
+  mutationThroughput: Metric.counter('mutation_throughput', {
+    description: 'Rate at which mutation operations are processed',
+    incremental: true,
+  }),
+  startupTime: Metric.timer('startup_time', 'Time to startup LiveStore'),
+  memoryUsage: Metric.gauge('memory_usage', { description: 'Memory usage during operations' }),
+  mainThreadBlocking: Metric.timer('main_thread_blocking', 'Main thread blocking duration'),
 })
 
-const mainThreadBlocking = Metric.timer('main_thread_blocking', 'Main thread blocking duration')
-
-const queryThroughput = Metric.counter('query_throughput', {
-  description: 'Rate at which query operations are processed',
-  incremental: true,
-})
-
-const mutationThroughput = Metric.counter('mutation_throughput', {
-  description: 'Rate at which mutation operations are processed',
-  incremental: true,
-})
-
-const startupTime = Metric.timer('startup_time', 'Time to startup LiveStore')
-
-const OTelLayer = OtelLiveHttp({ serviceName: 'livestore-performance' })
-
-export const MetricsLayer = Layer.succeed(PerformanceMetrics, {
-  queryLatency,
-  mutationLatency,
-  memoryUsage,
-  mainThreadBlocking,
-  queryThroughput,
-  mutationThroughput,
-  startupTime,
-}).pipe(Layer.provide(OTelLayer))
+export const TelemetryLive = Layer.merge(PerformanceMetricsLive, OtelLiveHttp({ serviceName: 'livestore-performance' }))
