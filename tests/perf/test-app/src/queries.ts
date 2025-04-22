0f094f5e5e24e8eb10b4ec55e60cd0c97ee6b2d7
Igor Gassmann <igor@igassmann.me>
2025-04-22 17:40:06 +0200

    query label casing

--- tests/perf/test-app/src/queries.ts
+++ tests/perf/test-app/src/queries.ts
@@ -4,4 +4,4 @@ import { tables } from './schema.js'
 
 export const uiState$ = queryDb(tables.uiState.get(), { label: 'uiState' })
 
-export const allItems$ = queryDb(tables.items.select(), { label: 'all-items' })
+export const allItems$ = queryDb(tables.items.select(), { label: 'allItems' })
