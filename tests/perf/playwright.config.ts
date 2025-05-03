c5d4e14eed21461214077facc31f36e1959bfdab
Igor Gassmann <igor@igassmann.me>
2025-05-03 12:22:18 +0200

    fix tsc call stack size exceeded error

--- tests/perf/playwright.config.ts
+++ tests/perf/playwright.config.ts
@@ -5,11 +5,11 @@ import * as process from 'node:process'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './dist/tests',
+  testDir: './tests',
   forbidOnly: !!process.env.CI, // Fail the build on CI if we accidentally left test.only in the source code
   workers: 1, // Run tests serially for more consistent performance measurements
   reportSlowTests: null,
-  reporter: [process.env.CI ? ['dot'] : ['line'], ['./dist/tests/measurements-reporter.js']],
+  reporter: [process.env.CI ? ['dot'] : ['line'], ['./tests/measurements-reporter.ts']],
   use: { baseURL: 'http://localhost:4173' },
   projects: [
     {
