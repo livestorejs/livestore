bb9979a421844e1bd05411d30f7343326cd12abb
Johannes Schickling <schickling.j@gmail.com>
2025-05-08 13:53:10 +0200

    fix playwright

--- tests/integration/src/tests/playwright/fixtures/vite.config.ts
+++ tests/integration/src/tests/playwright/fixtures/vite.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
     // TODO remove @livestore/wa-sqlite once fixed https://github.com/vitejs/vite/issues/8427
     // TODO figure out why `fsevents` is needed. Otherwise seems to throw error when starting Vite
     // Error: `No loader is configured for ".node" files`
-    exclude: ['@livestore/wa-sqlite', 'fsevents'],
+    exclude: [
+      '@livestore/wa-sqlite',
+      'fsevents',
+      'playwright-core', // Needed to avoid (https://share.cleanshot.com/z92cVCVD)
+      'lightningcss', // Needed to avoid (https://share.cleanshot.com/DtKNwNcQ)
+    ],
   },
   plugins: [
     TEST_LIVESTORE_SCHEMA_PATH_JSON
