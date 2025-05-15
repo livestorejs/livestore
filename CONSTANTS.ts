5fdd0c05ea90a42d9efba5bd6e35810ec77aa1d1
Johannes Schickling <schickling.j@gmail.com>
2025-05-15 11:53:49 +0200

    chore: add LIVESTORE_WA_SQLITE_VERSION constant and update documentation references

--- CONSTANTS.ts
+++ CONSTANTS.ts
@@ -2,4 +2,6 @@ export const EFFECT_VERSION = '3.14.15'
 export const REACT_VERSION = '19.0.0'
 export const MIN_NODE_VERSION = '23.0.0'
 
+export const LIVESTORE_WA_SQLITE_VERSION = '1.0.5-dev.2'
+
 export const DISCORD_INVITE_URL = 'https://discord.gg/RbMcjUAPd7'
--- docs/data.ts
+++ docs/data.ts
@@ -1,5 +1,7 @@
 import { execSync } from 'node:child_process'
 
+import { liveStoreVersion } from '@livestore/common'
+
 export const officeHours = [
   'https://www.youtube.com/embed/MenhU6n0r5c', // 2
   'https://www.youtube.com/embed/2GYKgI1GU8k', // 1
@@ -12,11 +14,13 @@ export const getBranchName = () => {
   return execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 }
 
+export const versionNpmSuffix = liveStoreVersion.includes('dev') ? `@${liveStoreVersion}` : ''
+
 export const IS_MAIN_BRANCH = process.env.GITHUB_REF_NAME
   ? process.env.GITHUB_REF_NAME === 'main'
   : getBranchName() === 'main'
 
 export const makeTiged = (example: string) => {
   const hashSuffix = IS_MAIN_BRANCH ? '' : `#${getBranchName()}`
-  return `bunx tiged --mode=git git@github.com:livestorejs/livestore/examples/standalone/${example}${hashSuffix} my-app`
+  return `bunx tiged --mode=git git@github.com:livestorejs/livestore/examples/standalone/${example}${hashSuffix} livestore-app`
 }
