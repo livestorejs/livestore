fc51a799426278635c43fe7e478a89a65eed9085
Johannes Schickling <schickling.j@gmail.com>
2025-05-16 21:08:30 +0200

    refactor: simplify getBranchName function and add office hours video link

--- docs/data.ts
+++ docs/data.ts
@@ -3,16 +3,12 @@ import { execSync } from 'node:child_process'
 import { liveStoreVersion } from '@livestore/common'
 
 export const officeHours = [
+  'https://www.youtube.com/embed/_VDSqi3k-gE', // 3
   'https://www.youtube.com/embed/MenhU6n0r5c', // 2
   'https://www.youtube.com/embed/2GYKgI1GU8k', // 1
 ]
 
-export const getBranchName = () => {
-  if (process.env.GITHUB_REF_NAME) {
-    return process.env.GITHUB_REF_NAME
-  }
-  return execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
-}
+export const getBranchName = () => execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
 export const versionNpmSuffix = liveStoreVersion.includes('dev') ? `@${liveStoreVersion}` : ''
 
