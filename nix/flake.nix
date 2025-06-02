f03a06dfc12b3130eb4b01267168e07903795089
Johannes Schickling <schickling.j@gmail.com>
2025-06-02 14:50:40 +0200

    chore: release 0.3.1-dev.0

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     playwright-web-flake = {
