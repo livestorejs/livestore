69df6e7a01ade221fbde0e552840c0e789573adb
Johannes Schickling <schickling.j@gmail.com>
2025-07-15 15:05:09 +0200

    chore: standardize string quotes in setup script
    - Updated string quotes in the setup script from double quotes to single quotes for consistency.
    - Ensured uniformity in log messages for improved readability during dependency installation and TypeScript build execution.
    
    These changes aim to enhance code consistency and maintainability in the setup process.

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
-import { $ } from "bun"
-import { existsSync } from "node:fs"
+import { existsSync } from 'node:fs'
+import { $ } from 'bun'
 
 process.env.FORCE_COLOR = '1'
 
@@ -9,7 +9,7 @@ $.cwd(process.env.WORKSPACE_ROOT!)
 
 const WORKSPACE_ROOT = process.env.WORKSPACE_ROOT!
 const lastGitHashFile = `${WORKSPACE_ROOT}/node_modules/.last_git_hash`
-const lastGitHash = existsSync(lastGitHashFile) ? await $`cat ${lastGitHashFile} 2>/dev/null`.text() : "no-git"
+const lastGitHash = existsSync(lastGitHashFile) ? await $`cat ${lastGitHashFile} 2>/dev/null`.text() : 'no-git'
 
 const currentGitHash = await $`git rev-parse HEAD 2>/dev/null || echo "no-git"`.text()
 
@@ -18,14 +18,14 @@ if (lastGitHash !== currentGitHash) {
   const parentDir = (await $`dirname "$PWD"`.text()).trim()
   const parentIsGitRepo = await $`git -C ${parentDir} rev-parse --is-inside-work-tree >/dev/null`.nothrow().quiet()
   if (parentIsGitRepo.exitCode !== 0) {
-    console.log("Installing node dependencies via pnpm...")
+    console.log('Installing node dependencies via pnpm...')
     await $`CI=1 pnpm install`
   } else {
     console.log(`Parent (${parentDir}) is a git repo, skipping node dependencies installation`)
   }
 
   // Run an initial TS build
-  console.log("Running initial TS build...")
+  console.log('Running initial TS build...')
   await $`mono ts`
 
   // Update the last git hash
