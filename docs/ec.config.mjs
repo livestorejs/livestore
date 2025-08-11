c035d3cc2272280e6585fa5a7c663ba00951cea6
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-08-11 05:30:24 -0400

    Get Twoslash working in the docs (#549)
    * get twoslash working in the docs
    
    * add sample twoslash code file
    
    * fix
    
    * remove ts-expect-error
    
    * test: empty commit to test CI stability
    
    ---------
    
    Co-authored-by: Johannes Schickling <schickling.j@gmail.com>

---
+++ docs/ec.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import { defineEcConfig } from 'astro-expressive-code'
+import ecTwoSlash from 'expressive-code-twoslash'
+import ts from 'typescript'
+
+export default defineEcConfig({
+  plugins: [
+    ecTwoSlash({
+      twoslashOptions: {
+        compilerOptions: {
+          allowImportingTsExtensions: true,
+          moduleResolution: ts.ModuleResolutionKind.Bundler,
+        },
+      },
+    }),
+  ],
+})
