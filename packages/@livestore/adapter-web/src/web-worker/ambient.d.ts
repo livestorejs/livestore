8248fbb5a2e8757ab12c8be75f1db1a82d91aa0e
Igor Gassmann <igor@igassmann.me>
2025-09-17 15:58:59 +0200

    refactor: OPFS

--- packages/@livestore/adapter-web/src/web-worker/ambient.d.ts
+++ packages/@livestore/adapter-web/src/web-worker/ambient.d.ts
@@ -1,23 +1,3 @@
-// TODO remove OPFS stuff
-// https://developer.mozilla.org/en-US/docs/Web/API/FileSystemSyncAccessHandle
-interface FileSystemSyncAccessHandle {
-  close: () => void
-  flush: () => Promise<void>
-  getSize: () => number
-  read: (buffer: ArrayBuffer, options?: FileSystemReadWriteOptions) => number
-  truncate: (newSize: number) => void
-  write: (buffer: ArrayBuffer, options?: FileSystemReadWriteOptions) => number
-  seek: (offset: number) => void
-}
-
-interface FileSystemReadWriteOptions {
-  at?: number
-}
-
-interface FileSystemFileHandle {
-  createSyncAccessHandle: () => Promise<FileSystemSyncAccessHandle>
-}
-
 // TODO bring back when Vite limitation is resolved https://github.com/vitejs/vite/issues/8427
 // declare module '*?sharedworker' {
 //   const sharedWorkerConstructor: {
