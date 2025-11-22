162fb51c78ed65eb30717b199b09447edec2ff60
Rich Harris <rich.harris@vercel.com>
2025-11-22 16:58:17 -0500

    tweaks

--- examples/web-todomvc-svelte/src/components/Header.svelte
+++ examples/web-todomvc-svelte/src/components/Header.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-  import { events } from '../livestore/schema.js'
-  import { uiState$ } from '../livestore/queries.js'
-  import { store } from '../livestore/store';
+	import { events } from '../livestore/schema.js';
+	import { uiState$ } from '../livestore/queries.js';
+	import { store } from '../livestore/store';
 
-  let { newTodoText } = $derived(store.query(uiState$))
-  const updatedNewTodoText = (text: string) => store.commit(events.uiStateSet({ newTodoText: text }))
-  const todoCreated = () =>
-    store.commit(
-      events.todoCreated({ id: crypto.randomUUID(), text: newTodoText }),
-      events.uiStateSet({ newTodoText: '' }),
-    )
+	let { newTodoText } = $derived(store.query(uiState$));
 </script>
 
 <header class="header">
-  <h1>TodoMVC</h1>
-  <input
-    class="new-todo"
-    placeholder="What needs to be done?"
-    autofocus
-    bind:value={() => newTodoText, (v) => updatedNewTodoText(v)}
-    onkeydown={(e) => {
-      if (e.key === 'Enter') {
-        todoCreated()
-      }
-    }}
-  />
+	<h1>TodoMVC</h1>
+	<input
+		class="new-todo"
+		placeholder="What needs to be done?"
+		autofocus
+		bind:value={
+			() => newTodoText,
+			(v) => store.commit(events.uiStateSet({ newTodoText: v }))
+		}
+		onkeydown={(e) => {
+			if (e.key === 'Enter') {
+				store.commit(
+					events.todoCreated({ id: crypto.randomUUID(), text: newTodoText }),
+					events.uiStateSet({ newTodoText: '' })
+				);
+			}
+		}}
+	/>
 </header>
