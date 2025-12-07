fec245926e2e66d71238e1bbf99591b5e5703f88
Victor Nyberg <contact@victornyberg.com>
2025-12-07 18:10:17 +1100

    event types

--- packages/@livestore/common/src/leader-thread/stream-events.ts
+++ packages/@livestore/common/src/leader-thread/stream-events.ts
@@ -73,97 +73,99 @@ export const streamEventsWithSyncState = ({
         Effect.forkScoped,
       )
 
-      return Stream.paginateChunkEffect({ cursor: initialCursor, head: EventSequenceNumber.Client.ROOT }, ({ cursor, head }) =>
-        Effect.gen(function* () {
-          /**
-           * Early check guards agains:
-           * since === until : Prevent empty query
-           * since > until : Incorrectly inverted interval
-           */
-          if (options.until && EventSequenceNumber.Client.isGreaterThanOrEqual(cursor, options.until)) {
-            return [Chunk.empty(), Option.none()]
-          }
+      return Stream.paginateChunkEffect(
+        { cursor: initialCursor, head: EventSequenceNumber.Client.ROOT },
+        ({ cursor, head }) =>
+          Effect.gen(function* () {
+            /**
+             * Early check guards agains:
+             * since === until : Prevent empty query
+             * since > until : Incorrectly inverted interval
+             */
+            if (options.until && EventSequenceNumber.Client.isGreaterThanOrEqual(cursor, options.until)) {
+              return [Chunk.empty(), Option.none()]
+            }
 
-          /**
-           * There are two scenarios where we take the next head from the headQueue:
-           *
-           * 1. We need to wait for the head to advance
-           * The Stream suspends until a new head is available on the headQueue
-           *
-           * 2. Head has advanced during itteration
-           * While itterating towards the lastest head taken from the headQueue
-           * in increments of batchSize it's possible the head could have
-           * advanced. This leads to a suboptimal amount of queries. Therefor we
-           * check if the headQueue is full which tells us that there's a new
-           * head available to take. Example:
-           *
-           * batchSize: 2
-           *
-           * --> head at: e3
-           * First query: e0 -> e2 (two events)
-           * --> head advances to: e4
-           * Second query: e2 -> e3 (one event but we could have taken 2)
-           * --> Take the new head of e4
-           * Third query: e3 -> e4 (unnecessary third query)
-           *
-           *
-           * To define the target, which will be used as the temporary until
-           * marker for the eventlog query, we select the lowest of three possible values:
-           *
-           * hardStop: A user supplied until marker
-           * current cursor + batchSize: A batchSize step towards the latest head from headQueue
-           * head: The latest head from headQueue
-           */
-          const waitForHead = EventSequenceNumber.Client.isGreaterThanOrEqual(cursor, head)
-          const headHasAdvanced = yield* Queue.isFull(headQueue)
-          const nextHead = (waitForHead ?? headHasAdvanced) ? yield* Queue.take(headQueue) : head
-          const hardStop = options.until?.global ?? Number.POSITIVE_INFINITY
-          const target = EventSequenceNumber.Client.Composite.make({
-            global: Math.min(hardStop, cursor.global + batchSize, nextHead.global),
-            client: EventSequenceNumber.Client.DEFAULT,
-          })
+            /**
+             * There are two scenarios where we take the next head from the headQueue:
+             *
+             * 1. We need to wait for the head to advance
+             * The Stream suspends until a new head is available on the headQueue
+             *
+             * 2. Head has advanced during itteration
+             * While itterating towards the lastest head taken from the headQueue
+             * in increments of batchSize it's possible the head could have
+             * advanced. This leads to a suboptimal amount of queries. Therefor we
+             * check if the headQueue is full which tells us that there's a new
+             * head available to take. Example:
+             *
+             * batchSize: 2
+             *
+             * --> head at: e3
+             * First query: e0 -> e2 (two events)
+             * --> head advances to: e4
+             * Second query: e2 -> e3 (one event but we could have taken 2)
+             * --> Take the new head of e4
+             * Third query: e3 -> e4 (unnecessary third query)
+             *
+             *
+             * To define the target, which will be used as the temporary until
+             * marker for the eventlog query, we select the lowest of three possible values:
+             *
+             * hardStop: A user supplied until marker
+             * current cursor + batchSize: A batchSize step towards the latest head from headQueue
+             * head: The latest head from headQueue
+             */
+            const waitForHead = EventSequenceNumber.Client.isGreaterThanOrEqual(cursor, head)
+            const headHasAdvanced = yield* Queue.isFull(headQueue)
+            const nextHead = (waitForHead ?? headHasAdvanced) ? yield* Queue.take(headQueue) : head
+            const hardStop = options.until?.global ?? Number.POSITIVE_INFINITY
+            const target = EventSequenceNumber.Client.Composite.make({
+              global: Math.min(hardStop, cursor.global + batchSize, nextHead.global),
+              client: EventSequenceNumber.Client.DEFAULT,
+            })
 
-          /**
-           * Eventlog.getEventsFromEventlog returns a Chunk from each
-           * query which is what we emit at each itteration.
-           */
-          const chunk = yield* Eventlog.getEventsFromEventlog({
-            dbEventlog,
-            options: {
-              ...options,
-              since: cursor,
-              until: target,
-            },
-          })
+            /**
+             * Eventlog.getEventsFromEventlog returns a Chunk from each
+             * query which is what we emit at each itteration.
+             */
+            const chunk = yield* Eventlog.getEventsFromEventlog({
+              dbEventlog,
+              options: {
+                ...options,
+                since: cursor,
+                until: target,
+              },
+            })
 
-          /**
-           * We construct the state for the following itteration of the stream
-           * loop by setting the current target as the since cursor and pass
-           * along the latest head.
-           *
-           * If we have the reached the user supplied until marker we signal the
-           * finalization of the stream by passing Option.none() instead.
-           */
-          const reachedUntil =
-            options.until !== undefined && EventSequenceNumber.Client.isGreaterThanOrEqual(target, options.until)
+            /**
+             * We construct the state for the following itteration of the stream
+             * loop by setting the current target as the since cursor and pass
+             * along the latest head.
+             *
+             * If we have the reached the user supplied until marker we signal the
+             * finalization of the stream by passing Option.none() instead.
+             */
+            const reachedUntil =
+              options.until !== undefined && EventSequenceNumber.Client.isGreaterThanOrEqual(target, options.until)
 
-          const nextState: Option.Option<{
-            cursor: EventSequenceNumber.Client.Composite
-            head: EventSequenceNumber.Client.Composite
-          }> = reachedUntil ? Option.none() : Option.some({ cursor: target, head: nextHead })
+            const nextState: Option.Option<{
+              cursor: EventSequenceNumber.Client.Composite
+              head: EventSequenceNumber.Client.Composite
+            }> = reachedUntil ? Option.none() : Option.some({ cursor: target, head: nextHead })
 
-          const spanAttributes = {
-            'livestore.streamEvents.cursor.global': cursor.global,
-            'livestore.streamEvents.target.global': target.global,
-            'livestore.streamEvents.batchSize': batchSize,
-            'livestore.streamEvents.waitedForHead': waitForHead,
-          }
+            const spanAttributes = {
+              'livestore.streamEvents.cursor.global': cursor.global,
+              'livestore.streamEvents.target.global': target.global,
+              'livestore.streamEvents.batchSize': batchSize,
+              'livestore.streamEvents.waitedForHead': waitForHead,
+            }
 
-          return yield* Effect.succeed<[Chunk.Chunk<LiveStoreEvent.Client.Encoded>, typeof nextState]>([
-            chunk,
-            nextState,
-          ]).pipe(Effect.withSpan('@livestore/common:streamEvents:segment', { attributes: spanAttributes }))
-        }),
+            return yield* Effect.succeed<[Chunk.Chunk<LiveStoreEvent.Client.Encoded>, typeof nextState]>([
+              chunk,
+              nextState,
+            ]).pipe(Effect.withSpan('@livestore/common:streamEvents:segment', { attributes: spanAttributes }))
+          }),
       )
     }),
   )
