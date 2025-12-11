139fe9ca398c6f205659b587cd7db754ae422924
Igor Gassmann <igor@igassmann.me>
2025-12-11 14:55:02 +0100

    test(integration): migrate to new multi-store API

--- tests/integration/src/tests/playwright/fixtures/devtools/todomvc/components/MainSection.tsx
+++ tests/integration/src/tests/playwright/fixtures/devtools/todomvc/components/MainSection.tsx
@@ -1,10 +1,9 @@
 /** biome-ignore-all lint/a11y: testing */
 import { queryDb } from '@livestore/livestore'
-import { useStore } from '@livestore/react'
 import React from 'react'
-
 import { uiState$ } from '../livestore/queries.ts'
 import { events, tables } from '../livestore/schema.ts'
+import { useAppStore } from '../livestore/store.ts'
 
 const visibleTodos$ = queryDb(
   (get) => {
@@ -18,7 +17,7 @@ const visibleTodos$ = queryDb(
 )
 
 export const MainSection: React.FC = () => {
-  const { store } = useStore()
+  const store = useAppStore()
 
   const toggleTodo = React.useCallback(
     ({ id, completed }: typeof tables.todos.Type) =>
