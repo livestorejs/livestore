3c077d57b9122e351b4539df7dfaadf1c3968ecf
Igor Gassmann <igor@igassmann.me>
2025-12-11 14:51:01 +0100

    refactor(web-todomvc-sync-electric): migrate to new multi-store API

--- examples/web-todomvc-sync-electric/src/routes/__root.tsx
+++ examples/web-todomvc-sync-electric/src/routes/__root.tsx
@@ -1,51 +1,25 @@
 import 'todomvc-app-css/index.css'
 
-import { makeAdapter } from '@livestore/adapter-node'
-import { makePersistedAdapter } from '@livestore/adapter-web'
-
-// can remain static import
-import LiveStoreSharedWorker from '@livestore/adapter-web/shared-worker?sharedworker'
-import { LiveStoreProvider } from '@livestore/react'
+import { StoreRegistry, StoreRegistryProvider } from '@livestore/react'
 import { createRootRoute, HeadContent, Outlet, Scripts } from '@tanstack/react-router'
-import { createIsomorphicFn } from '@tanstack/react-start'
 import type * as React from 'react'
-import { unstable_batchedUpdates as batchUpdates } from 'react-dom'
+import { Suspense, useState } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 import { VersionBadge } from '../components/VersionBadge.tsx'
-import { SyncPayload, schema } from '../livestore/schema.ts'
-import LiveStoreWorker from '../livestore.worker.ts?worker'
-import { getStoreId } from '../util/store-id.ts'
-
-// module level vars are kept across requests
-const getAdapter = createIsomorphicFn()
-  .server(() => makeAdapter({ storage: { type: 'in-memory' } }))
-  .client(() =>
-    makePersistedAdapter({ storage: { type: 'opfs' }, worker: LiveStoreWorker, sharedWorker: LiveStoreSharedWorker }),
-  )
-
-// TODO: otel support for tanstack start
-
-const adapter = getAdapter()
 
 const RootComponent = () => {
-  const storeId = getStoreId()
+  const [registry] = useState(() => new StoreRegistry())
 
   return (
     <RootDocument>
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <LiveStoreProvider
-          schema={schema}
-          storeId={storeId}
-          renderLoading={() => <div>Loading...</div>}
-          adapter={adapter}
-          batchUpdates={batchUpdates}
-          syncPayloadSchema={SyncPayload}
-          syncPayload={{ authToken: 'insecure-token-change-me' }}
-        >
-          <Outlet />
-          <VersionBadge />
-        </LiveStoreProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <StoreRegistryProvider storeRegistry={registry}>
+            <Outlet />
+            <VersionBadge />
+          </StoreRegistryProvider>
+        </Suspense>
       </ErrorBoundary>
     </RootDocument>
   )
