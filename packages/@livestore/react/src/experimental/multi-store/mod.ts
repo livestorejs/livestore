f376d115f891f22524a521ef7d643eba33ba0722
Igor Gassmann <igor@igassmann.me>
2025-12-12 15:07:57 +0100

    refactor(react): remove experimental/multi-store exports

--- packages/@livestore/react/src/experimental/multi-store/mod.ts
+++
@@ -1,5 +0,0 @@
-// Re-export from @livestore/livestore for backwards compatibility
-export { type CachedStoreOptions, StoreRegistry, storeOptions } from '@livestore/livestore'
-// Re-export React hooks/components for convenience
-export { StoreRegistryProvider, useStoreRegistry } from '../../StoreRegistryContext.tsx'
-export { useStore } from '../../useStore.ts'
