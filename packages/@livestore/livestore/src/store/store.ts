704aa66c9fcd244d73947b1d823d736b8a54b550
Igor Gassmann <igor@igassmann.me>
2026-06-18 20:17:30 +0200

    refactor!: replace `Schema.Codec<T>` with `Schema.Schema<T>`

--- packages/@livestore/livestore/src/store/store.ts
+++ packages/@livestore/livestore/src/store/store.ts
@@ -609,7 +609,7 @@ export class Store<TSchema extends LiveStoreSchema = LiveStoreSchema.Any, TConte
    * ```
    */
   query = <TResult>(
-    query: Queryable<TResult> | { query: string; bindValues: Bindable; schema?: Schema.Codec<TResult> },
+    query: Queryable<TResult> | { query: string; bindValues: Bindable; schema?: Schema.Schema<TResult> },
     options?: { otelContext?: otel.Context; debugRefreshReason?: RefreshReason },
   ): TResult => {
     this.checkShutdown('query')
