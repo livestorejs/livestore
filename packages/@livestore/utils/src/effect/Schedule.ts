b15d5108befd305bcde52e41a81836ef4735ff8d
Igor Gassmann <igor@igassmann.me>
2026-06-20 15:45:37 +0200

    chore(utils)!: update schedule delay migration for Effect v4

--- packages/@livestore/utils/src/effect/Schedule.ts
+++ packages/@livestore/utils/src/effect/Schedule.ts
@@ -1,9 +1,9 @@
-import { Duration, pipe, Schedule } from 'effect'
+import { Duration, Effect, pipe, Schedule } from 'effect'
 
 export * from 'effect/Schedule'
 
 export const exponentialBackoff10Sec = pipe(
   Schedule.exponential(Duration.millis(10), 4), // 10ms, 40ms, 160ms, 640ms, 2560ms, ...
-  Schedule.modifyDelay((_, delay) => Duration.min(delay, Duration.seconds(1))),
+  Schedule.modifyDelay((_, delay) => Effect.succeed(Duration.min(delay, Duration.seconds(1)))),
   Schedule.bothLeft(Schedule.during(Duration.seconds(10))), // max 10 seconds
 )
