ce83bfe16d294ce7fefbcbcb8289cdac92ff3335
Igor Gassmann <igor@igassmann.me>
2026-07-07 18:13:33 +0200

    fix: repair perf eventlog test app tsconfig

--- tests/perf-eventlog/test-app/tsconfig.json.genie.ts
+++ tests/perf-eventlog/test-app/tsconfig.json.genie.ts
@@ -1,11 +1,13 @@
-import { tsconfigJson } from '../../../genie/repo.ts'
+import { domLib, tsconfigJson } from '../../../genie/repo.ts'
 import baseTsconfig from '../tsconfig.json.genie.ts'
 
 export default tsconfigJson({
-  ...baseTsconfig,
+  ...baseTsconfig.data,
   compilerOptions: {
-    ...baseTsconfig.compilerOptions,
+    ...baseTsconfig.data.compilerOptions,
+    lib: [...domLib],
     types: ['vite/client'],
   },
   include: ['./src', './vite.config.ts', './index.html', '../src'],
+  references: baseTsconfig.data.references.map((reference) => ({ ...reference, path: `../${reference.path}` })),
 })
